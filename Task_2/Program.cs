ff2a01ce0a066ebee8a9c3e25bce57c34ef70153
SergeyShamin <shamins61@yandex.ru>
2022-04-28 20:50:44 +0300

    done Task 4

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -3,4 +3,50 @@
 3 -> Среда
 5 -> Пятница
 */
-int number = Console ();
+
+Console.WriteLine("Введите число от 1 до 7 ");
+
+int number = Convert.ToInt32 (Console.ReadLine());
+switch (number)
+{
+    case 1:
+        {
+            System.Console.WriteLine("Понедельник");
+            break;
+        }
+    case 2:
+        {
+            System.Console.WriteLine("Вторник");
+            break;
+        }
+    case 3:
+        {
+            System.Console.WriteLine("Среда");
+            break;
+        }
+    case 4:
+        {
+            System.Console.WriteLine("Четверг"); 
+            break;
+        }
+    case 5:
+        {
+            System.Console.WriteLine("Пятница");       
+            break;
+        }
+    case 6:
+        {
+            System.Console.WriteLine("Суббота"); 
+            break;
+        }
+    case 7:
+        {
+            System.Console.WriteLine("Воскресенье");
+            break;
+        }
+    default:
+        {
+            System.Console.WriteLine("Дней недели всего 7");
+            break;
+        }
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Напишите програму вычисления значения
+функции возведения числа в квадрат.
+4 в 16
+3 в 9
+7 в 49
+*/
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine (number * number);
